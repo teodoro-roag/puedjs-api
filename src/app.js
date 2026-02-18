a83ce2205f80ece6cf4fd6e27c3dadf9700e3cfa
teodoro-roag <teodororoag@gmail.com>
2026-02-18 12:33:12 -0600

    Estructura de proyecto y configuracion base

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const usuariosRoutes = require('./routes/usuarios.routes');
+const eventosRoutes = require('./routes/eventos.routes');
+const asistenciaRoutes = require('./routes/asistencia.routes');
+const swagRoutes = require('./routes/swag.routes');
+const canjeRoutes = require('./routes/canje.routes');
+const errorHandler = require('./middlewares/error.middleware');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/v1/usuarios', usuariosRoutes);
+app.use('/api/v1/eventos', eventosRoutes);
+app.use('/api/v1/asistencias', asistenciaRoutes);
+app.use('/api/v1/swag', swagRoutes);
+app.use('/api/v1/canje', canjeRoutes);
+
+app.use(errorHandler);
+
+module.exports = app;
